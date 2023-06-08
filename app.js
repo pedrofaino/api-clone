e31d4c8803e5a21a802f9d1c04249b17012a18fb
pedrofaino <pedrofaino99@gmail.com>
2023-06-08 14:35:17 -0300

    deploy

---
+++ app.js
@@ -0,0 +1,57 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
+import authRouter from "./src/routes/auth.route.js"
+import routerUsers from "./src/routes/users.js";
+import paymentRouter from "./src/routes/payment.route.js";
+import routerFavs from "./src/routes/favs.js";
+import { connectDB } from './src/database.js';
+import routerAcc from "./src/routes/accommodation.js";
+
+const app = express();
+
+const whiteList = [process.env.ORIGIN1]
+
+app.use(cors({
+    origin:function(origin, callback){
+        if(whiteList.includes(origin)){
+            return callback(null, origin)
+        }
+        return callback("Error de CORS origin: "+origin+" no autorizado.")
+    },
+    credentials:true,
+    })
+);
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/users", routerUsers);
+app.use('/api/auth', authRouter);
+app.use('/api/payment', paymentRouter);
+app.use('/api/accommodation', routerAcc)
+app.use('/api/favourites', routerFavs);
+
+// in case of using another route
+app.all("*", (req, res) => {
+    res.status(404).json({ error: "404 Not Found" });
+});
+
+const PORT = process.env.PORT;
+
+connectDB().then(() => {
+    const server = app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+
+    server.on("error", (error) => {
+        console.log("Error starting Express server: " + error.message);
+        process.exit(1);
+    });
+}).catch((error) => {
+    console.error("Error connecting to database: " + error.message);
+    process.exit(1);
+});
